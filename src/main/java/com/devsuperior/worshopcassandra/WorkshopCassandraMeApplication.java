e3eb122afb9f5602784136b1336c5fc066225611
Cristiano Araujo <crisd.silva75@gmail.com>
2025-09-09 15:23:44 -0300

    Start project

---
+++ src/main/java/com/devsuperior/worshopcassandra/WorkshopCassandraMeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.worshopcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopCassandraMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopCassandraMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/worshopcassandra/WorkshopCassandraMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.worshopcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopCassandraMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
